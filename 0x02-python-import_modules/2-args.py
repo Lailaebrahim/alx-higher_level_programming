f4b43187c439099aee98af509b7dc8a9280a89b2
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-10-11 15:45:24 -0700

    fix pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if n == 0:
         print("0 arguments.")
     elif n == 1:
-         print("1 argument:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(n))
     for i in range(n):
