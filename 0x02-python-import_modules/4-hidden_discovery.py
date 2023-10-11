1726cbe513b43872a47e4fbec212f0352f839342
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-10-11 16:02:58 -0700

    task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,7 @@
 
 if __name__ == "__main__":
 
-    import hidden_4
+    from hidden_4 import *
     names = dir(hidden_4)
     for name in names:
-        if name[:2] != "__":
-            print(name)
+        print(name)
