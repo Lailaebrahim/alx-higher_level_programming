12df891e0fe6777938bdd4f2d5706047a429d7c8
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-10-24 18:27:01 -0700

    fix

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -28,6 +28,7 @@ class Square:
     def area(self):
         """Return the current area of the square."""
         return (self.__size)**2
+
     def my_print(self):
         """Print the square with the # character."""
         for i in range(0, self.__size):
