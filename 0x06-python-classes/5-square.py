e70cfcc35a6c8325897bd38e65c5183c59f12a12
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-10-24 18:25:21 -0700

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""A module for square"""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size=0):
+        """Constructor.
+        Args:
+            size: length of side of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set the current size of the square."""
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size)**2
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+            if self.__size == 0:
+                print("")
