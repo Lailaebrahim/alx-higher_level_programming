8fe634157041fc6247b0c319349eec3057eaab0e
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-10-30 10:28:03 -0700

    Task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module for print_square method.
+   This module contain only print_square method
+   which print a square of hashtages
+"""
+
+
+def print_square(size):
+    """Method for printing a square with # characters.
+
+    Args:
+        size: an int size of the square's side.
+
+    Raises:
+        TypeError: If size is not an int.
+        ValueError: If size is less than 0.
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
