2f615c1347e59a8d2fd268ea8d058d0f67fb74c3
lailaebrahim <ebrahimlaila815@gmail.com>
2023-11-09 00:49:01 +0200

    Task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module to define a new SubClass of Superclass list """
+
+
+class MyList(list):
+    """A class to make a subclass of list class
+
+   Methods:
+        print_sorted :Method to sort list and printed
+    """
+    def print_sorted(self):
+        self.print_sorted()
