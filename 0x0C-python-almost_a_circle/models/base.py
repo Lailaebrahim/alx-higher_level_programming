f52dc54f9abc37d926114b784680069d311f3592
lailaebrahim <ebrahimlaila815@gmail.com>
2023-11-10 14:32:14 +0200

    Upload Repo

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for Base class"""
+
+
+class Base:
+    """
+    A base class This class will be the “base” of all other classes in this project.
+    The goal of it is to manage id attribute in all future classes
+    and to avoid duplicating the same code.
+
+    Attributes:
+        __nb_objects (int): to store number of objects.
+    """
+
+    __nb_objects = 0
+
+    def __int__(self, id=None):
+        """Initialize a new Base object.
+
+        Args:
+             id (int): a specific id for each new object.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Module for Rectangle class which is a subclass of Base class"""
+from models.base import Base
+
+
+class Rectangle(Base):
