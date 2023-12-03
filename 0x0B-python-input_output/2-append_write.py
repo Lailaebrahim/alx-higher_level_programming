0f435f579d01d958fa06d03400559608a38fe9f6
lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-03 18:00:20 +0200

    fix

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,7 +6,7 @@ Module to define a method that append to a file
 
 def write_file(filename="", text=""):
     """
-    Method to write to a text file
+    Method to append to a text file
     :param filename: a string that is the path of the text file to append to
     :param text: text to be appended
     :return: number of appended characters
