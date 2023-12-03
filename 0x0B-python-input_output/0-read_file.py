b6d5713e363a778cb6d4a9e6136e884865bd5f7a
lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-03 17:17:35 +0200

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module to define a mehthod that read from a file
+"""
+import os
+
+
+def read_file(filename=""):
+    """
+    Method to read from a text file
+    :param filename: a string that is the path of the text file to read from
+    :return: No return
+    """
+    with open(filename, "r") as file:
+        print(file.read(), end="")
