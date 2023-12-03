87d110d76ad21a935f97af68236dd0a5ec11da92
lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-03 20:13:05 +0200

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Module to returns the dictionary description of the obj
+    where all the keys or values are JSON serializable.
+"""
+
+
+class Student:
+    """
+    Represent a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor
+        :param first_name: first name of student
+        :param last_name: last name of student
+        :param age: age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        A method that retrieves a dictionary representation of a Student instance
+        If attrs is a list of strings, only attribute names contained
+        in this list must be retrieved.
+        Otherwise, all attributes must be retrieved
+        :param attrs:
+        :return:
+        """
+        dic = {}
+        if isinstance(attrs, list) and all(isinstance(item, str) for item in attrs):
+            for item in attrs:
+                if item in ['first_name', 'last_name', 'age']:
+                    if item == "first_name":
+                        dic.update(first_name=self.first_name)
+                    elif item == "last_name":
+                        dic.update(last_name=self.last_name)
+                    elif item == "age":
+                        dic.update(age=self.age)
+            return dic
+        else:
+            return self.__dict__
+
