b8fc50ec2f459538bbc243c764417a76a2215dac
lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-03 18:57:17 +0200

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Module that adds all arguments to a Python list,
-and then save them to a file
+Module that adds all arguments to a Python list and then
+save them to a file
 it load content by load_from_json_file module
 and load the new data by save_to_json_file.
 """
