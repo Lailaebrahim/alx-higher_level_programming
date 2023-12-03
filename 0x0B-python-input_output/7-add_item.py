0f067497ab64486e1107d568d95a7cd2e977f67a
lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-03 18:56:16 +0200

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,11 +9,11 @@ and load the new data by save_to_json_file.
 
 import sys
 
-
 if __name__ == "__main__":
     load_from_json_file = \
-        (__import__('6-load_from_json_file').load_from_json_file)
+        __import__('6-load_from_json_file').load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
 i = 0
 new_data = []
 try:
