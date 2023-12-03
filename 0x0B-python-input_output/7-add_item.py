da9295d9bd041a49173d779af6ccc2103cc94545
lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-03 18:45:41 +0200

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,13 +9,15 @@ if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 i = 0
 new_data = []
-with open("add_item.json", "a") as js_file:
+try:
     old_data = load_from_json_file("add_item.json")
-    for arg in sys.argv:
-        if i == 0:
-            continue
-        else:
-            new_data[i] = arg
-            i += 1
-    data = old_data.extend(new_data)
-    save_to_json_file("add_item.json", data)
+except FileNotFoundError:
+    old_data = []
+for arg in sys.argv:
+    if i == 0:
+        continue
+    else:
+        new_data[i] = arg
+        i += 1
+data = old_data.extend(new_data)
+save_to_json_file(data, "add_item.json")
