04ff3d4ca0fc27514c79086a8d23d23451eb366e
lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-03 19:09:54 +0200

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""Module to define a Method that
+Add all arguments to a Python list and save them to a file.
+"""
+
+
 import sys
 
 if __name__ == "__main__":
