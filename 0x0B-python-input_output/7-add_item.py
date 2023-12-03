a6b2cecefb4a18097c708b7ddc794081b97d8f6a
lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-03 19:08:30 +0200

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,17 +6,18 @@ if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
-i = 0
-new_data = []
-try:
-    old_data = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    old_data = []
-for arg in sys.argv:
-    if i == 0:
-        continue
-    else:
-        new_data[i] = arg
-        i += 1
-data = old_data.extend(new_data)
-save_to_json_file(data, "add_item.json")
+
+    i = 0
+    new_data = []
+    try:
+        old_data = load_from_json_file( "add_item.json" )
+    except FileNotFoundError:
+        old_data = []
+    for arg in sys.argv:
+        if i == 0:
+            continue
+        else:
+            new_data[i] = arg
+            i += 1
+    data = old_data.extend( new_data )
+    save_to_json_file( data, "add_item.json" )
