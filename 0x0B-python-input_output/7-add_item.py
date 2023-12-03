45cedcbc7b5b57f946f0d411abe0a3836b170855
lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-03 18:35:42 +0200

    Task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+
+"""
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+i = 0
+new_data = []
+with open("add_item.json", "a") as js_file:
+    old_data = load_from_json_file("add_item.json")
+    for arg in sys.argv:
+        if i == 0:
+            continue
+        else:
+            new_data[i] = arg
+            i += 1
+    data = old_data + new_data
+    save_to_json_file("add_item.json", data)
