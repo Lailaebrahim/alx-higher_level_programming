765ec941a3bd404cd14a16c8cd37b43e5e8a38ce
lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-03 19:00:15 +0200

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,7 +12,7 @@ import sys
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+        __import__( '6-load_from_json_file' ).load_from_json_file
 
 i = 0
 new_data = []
