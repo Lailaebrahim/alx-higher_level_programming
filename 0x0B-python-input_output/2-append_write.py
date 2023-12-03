cdea2c0dff858b14fffd822a5c143963544110c8
lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-03 17:43:17 +0200

    fix

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module to define a method that append to a file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Method to write to a text file
+    :param filename: a string that is the path of the text file to append to
+    :param text: text to be appended
+    :return: number of appended characters
+    """
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module to define a method that return the JSON representation of an object (string)
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Method that return the JSON representation of an object (string)
+    :param my_obj: object to return it's JSON representation
+    :return: JSON representation of an object (string)
+    """
+    return json.dumps(my_obj)
