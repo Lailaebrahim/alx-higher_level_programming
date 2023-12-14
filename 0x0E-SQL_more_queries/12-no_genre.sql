9544ca16aae0ad4ea883039ab7e91df210082c0d
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-14 12:57:40 -0800

    Task 13

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,6 @@
--- cat 11-genre_id_all_shows.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL
+
+FROM tv_genres
+RIGHT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.show_id
+GROUB BY GENRE
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
