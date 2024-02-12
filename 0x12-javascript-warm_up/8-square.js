b783362ef5741f9e923f523c1189f9e64f6ea718
Lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-12 12:58:03 -0800

    Task8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let num = parseInt(process.argv[2]); num > 0; num--) {
     let output = '';
