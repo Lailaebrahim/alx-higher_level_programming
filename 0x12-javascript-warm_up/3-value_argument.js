70378f1838ee2c600108cca96473cb8fb645ba25
Lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-14 15:18:48 -0800

    fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,9 @@
-#!/usr/bin/node
-if (process.argv.length === 2) {
+#!/usr/bin/env node
+
+const arg = process.argv[2];
+
+if (arg === undefined) {
   console.log('No argument');
 } else {
-  console.log(process.argv[2]);
+  console.log(arg);
 }
