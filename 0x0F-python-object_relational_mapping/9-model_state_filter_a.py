e3a2c877604ccf102cf9f9566fb77aed084c0f5e
laila <laila@ubuntu1.myguest.virtualbox.org>
2024-02-17 06:09:23 +0200

    fix

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,12 +8,12 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).first()
-    if instance is None:
-        print("Nothing")
-    else:
-        print(instance.id, ": ", instance.name)
+    for instance in session.query(State).order_by(State.id)[0:1]:
+        if instance is None:
+            print("Nothing")
+        else:
+            print(instance.id, ": ", instance.name)
