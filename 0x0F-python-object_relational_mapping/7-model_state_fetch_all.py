098463d66b25208ee148a588c1d887fa6012d605
laila <laila@ubuntu1.myguest.virtualbox.org>
2024-02-17 05:51:56 +0200

    Task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State):
+    for instance in session.query(State).order_by(State.id):
         print(instance.id, ": ", instance.name)
