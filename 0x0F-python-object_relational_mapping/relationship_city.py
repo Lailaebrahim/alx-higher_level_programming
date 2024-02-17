6e93de2786bb82c8d9f0a09d129e3f656d160bfe
Lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-17 15:58:04 -0800

    Working on Advanced Task

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module define state class"""
+from sqlalchemy import Column, String, Integer, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """class inherit from Base to create states table"""
+    __tablename__ = 'cities'
+    id = Column(Integer, nullable=False, unique=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
