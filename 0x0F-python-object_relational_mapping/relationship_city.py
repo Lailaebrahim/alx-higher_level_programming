ae487750d828f586942ec9c69e9011cc28de6072
lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-18 02:25:04 +0200

    Work on Adanced Task1

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -3,6 +3,7 @@
 from sqlalchemy import Column, String, Integer, ForeignKey
 from model_state import Base
 from sqlalchemy.orm import relationship
+from relationship_state import City
 
 
 class City(Base):
