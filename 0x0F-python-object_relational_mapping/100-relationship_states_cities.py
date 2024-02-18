55bd05c0ea758bf16ad0452c6d29a18dee144487
lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-18 02:38:51 +0200

    Work on Adanced Task1

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     new_state = State(name='California')
-    req_city = session.query(City).filter(City.name == "San Francisco")
-    req_city.state = new_state
-    session.add(new_state)
+    new_city = City(name="San Francisco")
+    new_state.cities.append(new_city)
+    session.add_all(new_state, new_city)
     session.commit()
