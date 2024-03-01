3a842390b88dd23e5b38cf7de05bd22b3aa04b9f
Lailaebrahim <lailaebrahim815@gmail.com>
2024-02-29 18:27:55 -0800

    Fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,4 +6,4 @@ def find_peak(list_of_integers):
     """Method to find peak"""
     if list_of_integers:
         list_of_integers.sort(reverse=True)
-        return list_of_integers
+        return list_of_integers[0]
