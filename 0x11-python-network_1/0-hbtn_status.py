80560f5636466e0fd0d2da0a972c2ca064e7a787
Lailaebrahim <lailaebrahim815@gmail.com>
2024-03-01 14:14:33 -0800

    Fix Syntax Error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,10 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with (urllib.request.urlopen("https://alx-intranet.hbtn.io/status")
-          as response):
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
         res = response.read()
         print("Body response:")
-        print("/t- type: {}".format(type(res)))
-        print("/t- content: {}".format(res))
-        print("/t- utf8 content:{}".format(res.decode('utf-8')))
+        print("    - type: {}".format(type(res)))
+        print("    - content: {}".format(res))
+        print("    - utf8 content:{}".format(res.decode('utf-8')))
