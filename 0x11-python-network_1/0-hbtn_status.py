9053e1e9df7f3d57f13452e1cdeb8a4acfbcc3b7
lailaebrahim <ebrahimlaila815@gmail.com>
2024-03-02 00:26:12 +0200

    Fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(res)))
         print("\t- content: {}".format(res))
-        print("\t- utf8 content:{}".format(res.decode('utf-8')))
+        print("\t- utf8 content: {}".format(res.decode('utf-8')))
